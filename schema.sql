cf0a96d14a1e4a7be687fb907121e57dabc42961
Jenna Dodge <jennadodge@Jennas-MacBook-Pro.local>
2022-01-31 17:29:43 -0700

    saved SQL queries

--- schema.sql
+++ schema.sql
@@ -52,35 +52,6 @@ CREATE TABLE titles(
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
-SELECT * FROM departments;
 
--- Retirement elgibility
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952_01_01' AND '1955-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952_01_01' AND '1952-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE (birth_date BETWEEN '1952_01_01' AND '1955-12-31') 
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT COUNT(first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952_01_01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Put results into a new table retirement_info
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952_01_01' AND '1955-12-31') 
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Now view the table
-SELECT * FROM retirement_info
 
 
